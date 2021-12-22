c67863b2690b47ac20055aa480c5087f436b8680
MHRumon <rumonuniv@gmail.com>
2021-12-22 12:51:34 +0900

    my-message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
-
+import ChatFeed from "./components/ChatFeed";
 // main chat app 
 const App = ()=>{
     return (
         <ChatEngine
             height='100vh'
             projectID = "a8749bcc-51df-4e92-b9ee-ecec63633220"
-            userName=""
-            userSecret=""
+            userName="rumonhasan"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/>}
         />
     )
 
